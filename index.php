3999ecbb8d6a9877b50f6db159277824c0b16510
Kontala Venkat Sai Kinshuk <109468444+kinshuk8@users.noreply.github.com>
2023-06-19 00:34:49 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="main.css">
+    <script src='main.js'></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+ </head>
+ <body>
+    <!--navbar-->
+    <nav class="navbar navbar-dark bg-primary navbar-expand-lg bg-body-primary" style="padding-bottom:15px;">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">CSO</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggle-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+               <ul class="navbar-nav">
+                  <li class="nav-item">
+                     <a href="#" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" aria-current="page">Home</a>
+                     <ul class="dropdown-menu">
+                     <li><a class="dropdown-item" href="http://cso.rvrjcce.ac.in/vision-mission.php">Vision & Mission</a></li>
+                     <li><a class="dropdown-item" href="http://cso.rvrjcce.ac.in/uc.php">HOD's Desk</a></li>
+                     <li><hr class="dropdown-divider"></li>
+                     </ul>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="https://rvrjc.ac.in">Main Website</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">People</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Academics</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Research</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Resources</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Student Activites</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link disabled">Disabled</a>
+                  </li>
+               </ul>
+            </div>
+        </div>
+    </nav>
+    <h3 style="text-align:center; background-color:blue; padding:30px;" > Program Overview </h3>
+   <img class="w3-image" style ="width: 100%; height: 400px;" src="images/cse-iot.png"/>
+
+   <div class="w3-container w3-card-4 w3-animate-opacity" style="margin: 15px;">
+
+      <p>Internet of Things (IoT) is among the latest innovations in the area of Computer Science & Engineering and IT. This specialization covers the development of IoT products and services along with the devices for sensing, actuating, processing and inter communication. The program aims to train the students and develop within them strong theoretical foundation, systematic professional knowledge and strong practical skills in the fields of web-technologies, real time embedded systems, communications protocols and collaborative intelligence.</p>
+      <p>The four - year programme 'B. Tech., CSE (IoT)' connects the academia and industries with the young minds in an effective way. It is an intensive and skilful program to utilize the potential and the innovative passion of students. The scheme of the programme is strongly aligned with the present and forthcoming requirements of the industries, and academia across the globe. Thus, it offers the ample of carrier opportunities to the students. In favour of the never ending process of learning and research, this programme meets the global standards of master's Programme in Artificial Intelligence, Machine Learning, Deep Learning, IoT, Smart Cities, Smart Healthcare and Smart Farming offered at the national and international universities of repute.</p>
+      <h4> Highlights </h4>
+      <ul>
+      <li>One of the top colleges in Andhra Pradesh is now offering best B.Tech Program in Internet of Things.</li>
+      <li>Builds a solid theoretical and strong practical skills in the fields of communication networks and IT that provides a wide range of applications in IoT through industry-oriented curriculum. </li>
+      <li>Learn through real time industry projects sponsored by global leaders in IoT industry and regular live sessions by industry experts. </li>
+      <li>Provides expertise in ARM based System on Chip design and Embedded system, sensors and instrumentation, edge and cloud computing , Digital Signal Processing etc.</li>
+      </ul>
+   </div>
+
+ </body>
+ </html>
